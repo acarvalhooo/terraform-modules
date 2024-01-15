3c82711c967f82d6787e76ac8af7cbfc7aac04aa
Alef Paulo Barbosa de Carvalho <datarain.alef@intelbras.com.br>
2024-01-15 11:12:35 -0300

    Adding SMB CSI Driver

---
+++ smb-csi-driver/README.md
@@ -0,0 +1,28 @@
+# Using the module via orchestrator (Example)
+```
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "smb-csi-driver" {
+  source                 = "./smb-csi-driver"
+  cluster-oidc-url       = "https://oidc.eks.us-east-1.amazonaws.com/id/A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0"
+  cluster-name           = "eks-cluster-sap-prod"
+  endpoint               = "https://A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0.A0A.us-east-1.eks.amazonaws.com"
+  cluster-ca-certificate = "Get this value from eks console identified by 'Certificate authority'"
+  chart-version          = "v1.13.0"
+  environment            = "prod"
+  project                = "sap"
+}
+```
+# Observation
+Access https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/master/charts for verify the chart version available
---
+++ smb-csi-driver/vars.tf
@@ -0,0 +1,55 @@
+# Cluster URL OIDC
+variable "cluster-oidc-url" {
+  description = "URL of cluster OIDC"
+  default     = ""
+  type        = string
+}
+
+# Cluster name
+variable "cluster-name" {
+  description = "Name of cluster that the controller will be installed"
+  default     = ""
+  type        = string
+}
+
+# API server endpoint
+variable "endpoint" {
+  description = "API server endpoint of cluster"
+  default     = ""
+  type        = string
+}
+
+# Cluster CA certificate
+variable "cluster-ca-certificate" {
+  description = "Cluster CA certificate"
+  default     = ""
+  type        = string
+}
+
+# OIDC issuer ARN
+variable "oidc-issuer-arn" {
+  description = "OIDC issuer arn"
+  default     = ""
+  type        = string
+}
+
+# Chart version
+variable "chart-version" {
+  description = "Chart version"
+  default     = ""
+  type        = string
+}
+
+# Environment
+variable "environment" {
+  description = "Environment where the resources are being launched"
+  default     = ""
+  type        = string
+}
+
+# Project
+variable "project" {
+  description = "Project name"
+  default     = ""
+  type        = string
+}
